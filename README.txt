4deb15c0e72885aea6056ba1a06605fc6aef3f71
Vanesa Reinerth <vanesa@getbowtied.com>
2019-11-29 12:35:56 +0200

    v.1.1

--- README.txt
+++ README.txt
@@ -2,7 +2,7 @@
 Contributors: getbowtied, vanesareinerth, adrianlbs
 Requires at least: 5.0
 Tested up to: 5.2.2
-Stable tag: 1.0
+Stable tag: 1.1
 Requires PHP: 5.5.0
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -15,6 +15,9 @@ Templates:
 - Contact Page
 
 == Changelog ==
+
+= 1.1 =
+- WordPress 5.3 compatibility updates
 
 = 1.0 =
 - Initial Version
--- mr-tailor-deprecated.php
+++ mr-tailor-deprecated.php
@@ -4,13 +4,13 @@
  * Plugin Name:       		Mr. Tailor Deprecated Features
  * Plugin URI:        		https://mrtailor.wp-theme.design/
  * Description:       		Old features of Mr. Tailor theme that are no longer used.
- * Version:           		1.0
+ * Version:           		1.1
  * Author:            		GetBowtied
  * Author URI:				https://getbowtied.com
  * Text Domain:				mr-tailor-deprecated
  * Domain Path:				/languages/
  * Requires at least: 		5.0
- * Tested up to: 			5.2.2
+ * Tested up to: 			5.3
  *
  * @package  Mr. Tailor Deprecated
  * @author   GetBowtied
@@ -51,7 +51,7 @@ if ( $theme->template == 'mrtailor') {
 	add_action( 'admin_enqueue_scripts', 'mr_tailor_deprecated_admin_styles' );
 	function mr_tailor_deprecated_admin_styles() {
 	    if ( is_admin() ) {
-	    	wp_enqueue_style( 
+	    	wp_enqueue_style(
 	    		'wpalchemy-metabox',
 	    		plugins_url( 'includes/metaboxes/assets/css/wp-admin-metabox.css', __FILE__ ),
 	    		array()
@@ -61,7 +61,7 @@ if ( $theme->template == 'mrtailor') {
 
 	add_action( 'wp_enqueue_scripts', 'mr_tailor_deprecated_styles' );
 	function mr_tailor_deprecated_styles() {
-    	wp_enqueue_style( 
+    	wp_enqueue_style(
     		'mr-tailor-deprecated',
     		plugins_url( 'includes/assets/css/style.css', __FILE__ ),
     		array()
@@ -76,59 +76,59 @@ if ( $theme->template == 'mrtailor') {
 		global $slider_metabox;
 		$slider_metabox->the_meta();
 
-		ob_start();	
+		ob_start();
 
 		$slide_counter = 0;
 
 		while( $slider_metabox->have_fields('items') ) { ?>
 
 			<?php $slide_counter++; ?>
-			
+
 			.main-slider .slide_<?php echo esc_html($slide_counter); ?> {
 				background-image:url(<?php echo ($slider_metabox->get_the_value('imgurl')); ?>);
 			}
-			
-			.main-slider .slide_<?php echo esc_html($slide_counter); ?> .main-slider-elements.animated {				
+
+			.main-slider .slide_<?php echo esc_html($slide_counter); ?> .main-slider-elements.animated {
 				-webkit-animation-name: <?php echo ($slider_metabox->get_the_value('slide_animation')); ?>;
 				-moz-animation-name: <?php echo ($slider_metabox->get_the_value('slide_animation')); ?>;
 				-o-animation-name: <?php echo ($slider_metabox->get_the_value('slide_animation')); ?>;
 				animation-name: <?php echo ($slider_metabox->get_the_value('slide_animation')); ?>;
 			}
-			
+
 			<?php if ($slider_metabox->get_the_value('slider_mood') == 'light') : ?>
 
 				.main-slider .slide_<?php echo esc_html($slide_counter); ?> h1 {
 					color:#000;
 				}
-				
+
 				.main-slider .slide_<?php echo esc_html($slide_counter); ?> h1:after {
 					background:#000;
 				}
-				
+
 				.main-slider .slide_<?php echo esc_html($slide_counter); ?> h2 {
 					color:#000;
 				}
-				
+
 				.main-slider .slide_<?php echo esc_html($slide_counter); ?> a.slider_button {
 					color:#fff;
 					background:#000;
 				}
-				
+
 				.main-slider .slide_<?php echo esc_html($slide_counter); ?> a.slider_button:hover {
 					color:#000 !important;
 					background:#fff !important;
 				}
-				
+
 				.main-slider .slide_<?php echo esc_html($slide_counter); ?> .arrow-left,
 				.main-slider .slide_<?php echo esc_html($slide_counter); ?> .arrow-right
 				{
 					color:#000;
 				}
-				
+
 			<?php endif; ?>
-			
-		<?php    
-		}	
+
+		<?php
+		}
 
 		$content = ob_get_clean();
 		wp_add_inline_style( 'mr-tailor-deprecated', $content );
